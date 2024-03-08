542cc6d337820605c496d88741fccfefc1d13e55
rryanjoness <ryangregoryjones2@gmail.com>
2024-03-08 13:48:19 -0500

    fixed starter errors in Rabbit

--- FoxesAndRabbits/src/Rabbit.java
+++ FoxesAndRabbits/src/Rabbit.java
@@ -1,3 +1,13 @@
+import java.util.List;
+
 public class Rabbit extends Animal {
 
+    public Rabbit(boolean alive, char sex, Location location){
+        super(alive, sex, location);
+    }
+
+
+    public void act(List<Animal> newAniimals){
+        
+    }
 }
